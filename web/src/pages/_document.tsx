ce08685182dddedbadf1581e4a1858fb18d3d039
raissa <raissa.campos26@gmail.com>
2022-11-02 17:46:41 -0300

    feat: structure jsx

---
+++ web/src/pages/_document.tsx
@@ -0,0 +1,26 @@
+//arquivo como se fosse o index.html do react, pois no next não vem esse arquivo
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="bg-gray-900 bg-app bg-no-repeat bg-cover">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
