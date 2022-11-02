c22675dcfafba71c8b46c6a8adb3bd0fc0614b33
raissa <raissa.campos26@gmail.com>
2022-11-02 15:35:22 -0300

    feat: route create code pool

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,44 @@
-import Fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-import cors from '@fastify/cors'
+import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
+import { z } from "zod";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
-    log: ['query']
-})
+  log: ["query"],
+});
 
 async function bootstrap() {
-   const fastify = Fastify({
-    logger: true
-   }) 
+  const fastify = Fastify({
+    logger: true,
+  });
 
-   await fastify.register(cors, {
-    origin: true //permite que todas as aplicações possam acessar, em prod o ideal é passar o dominio 'www...'
-   })
+  await fastify.register(cors, {
+    origin: true, //permite que todas as aplicações possam acessar, em prod o ideal é passar o dominio 'www...'
+  });
 
-   fastify.get('/pools/count', async () => {
-    const count = await prisma.pool.count()
-    return { count }
-   })
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
 
-   await fastify.listen({ port: 3333}) 
-//    host: '0.0.0.0'
+    const { title } = createPoolBody.parse(request.body); //pra fazer a validação e garantir que não haja campo nulo
 
+    const generate = new ShortUniqueId({ length: 6 }); //vai gerar um código unico de 6 digitos
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return reply.status(201).send({ code });
+  });
+
+  await fastify.listen({ port: 3333 });
+  //    host: '0.0.0.0'
 }
 
-bootstrap()
+bootstrap();
