ae0261debd146000c98ef8b3ab1b2155bce005f8
raissa <raissa.campos26@gmail.com>
2022-11-03 15:13:15 -0300

    feat: structure context login

---
+++ mobile/src/contexts/AuthContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, ReactNode } from "react";
+
+interface UserProps {
+  name: string;
+  avatarUrl: string;
+}
+
+export interface AuthContextDataProps {
+  user: UserProps;
+  signIn: () => Promise<void>;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthProviderProps) {
+  async function signIn() {
+    console.log(
+      "🚀 ~ file: AuthContext.tsx ~ line 23 ~ signIn ~ entrou",
+      "Vamos logar"
+    );
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signIn,
+        user: {
+          name: "Raissa",
+          avatarUrl: "https://github.com/raissacmp.png",
+        },
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
