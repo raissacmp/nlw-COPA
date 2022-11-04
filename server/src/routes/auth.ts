195c8fd384f4adec1872e3770c6dd8bcb652ab8f
raissa <raissa.campos26@gmail.com>
2022-11-04 16:36:39 -0300

    feat: organization filess auth

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,15 +1,20 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export async function authRoutes(fastify: FastifyInstance) {
   //get user logado
 
-  fastify.get("/me", async (request) => {
-    await request.jwtVerify();
-
-    return { user: request.user };
-  });
+  fastify.get(
+    "/me",
+    {
+      onRequest: [authenticate],
+    },
+    async (request) => {
+      return { user: request.user };
+    }
+  );
 
   fastify.post("/users", async (request) => {
     const createUserBody = z.object({
