9edbb1b6269f1d24c65cff687257aa2b61a46669
raissa <raissa.campos26@gmail.com>
2022-11-05 11:46:11 -0300

    feat: loader login

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,18 +1,17 @@
 import { Box } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
+import { useAuth } from "../hooks/useAuth";
 
 import { AppRoutes } from "./app.routes";
 import { SignIn } from "../screens/SignIn";
 
 export function Routes() {
-  //   const { user } = useAuth();
+  const { user } = useAuth();
 
   return (
     <Box flex={1} bgColor="gray.900">
       <NavigationContainer>
-        {/* {user.name ? <AppRoutes /> : <SignIn />} */}
-        <SignIn />
-        {/* <AppRoutes /> */}
+        {user.name ? <AppRoutes /> : <SignIn />}
       </NavigationContainer>
     </Box>
   );
