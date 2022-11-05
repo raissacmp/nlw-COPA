cdc738a0966c21a7dfc3b13626b84f05b0fe51d9
raissa <raissa.campos26@gmail.com>
2022-11-05 18:06:52 -0300

    feat: guesses and games

--- mobile/src/screens/Details.tsx
+++ mobile/src/screens/Details.tsx
@@ -90,8 +90,7 @@ export function Details() {
             />
           </HStack>
 
-          <Guesses poolId={poolDetails.id} />
-          {/* code={poolDetails.code} */}
+          <Guesses poolId={poolDetails.id} code={poolDetails.code} />
         </VStack>
       ) : (
         <EmptyMyPoolList code={poolDetails.code} />
