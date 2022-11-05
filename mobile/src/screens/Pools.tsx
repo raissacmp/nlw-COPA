3cdb020c06d3e48f6753c64d748fc0c21d20bbf3
raissa <raissa.campos26@gmail.com>
2022-11-05 14:24:35 -0300

    feat: list of pools interface

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,12 +1,44 @@
-import { VStack, Icon } from "native-base";
+import { useEffect, useState } from "react";
+import { VStack, Icon, useToast, FlatList } from "native-base";
 import { Octicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { PoolCard, PoolCardProps } from "../components/PoolCard";
+import { Loading } from "../components/Loading";
+
+import { api } from "../services/api";
+import { EmptyPoolList } from "../components/EmptyPoolList";
 
 export function Pools() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardProps[]>([]);
+
   const { navigate } = useNavigation();
+  const toast = useToast();
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/pools");
+
+      setPools(response.data.pools);
+    } catch (error) {
+      console.log(error);
+      toast.show({
+        title: "Não foi possível carregar os bolões! 😢",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchPools();
+  }, []);
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -28,6 +60,19 @@ export function Pools() {
           onPress={() => navigate("find")} //tipo um onClick, quando clicarmos vai nos levar pra rota na qual passamos como parametro
         />
       </VStack>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={pools}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <PoolCard data={item} />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+          px={5}
+          ListEmptyComponent={() => <EmptyPoolList />} //propriedade que renderiza o componente quando nmão tem conteúdo
+        />
+      )}
     </VStack>
   );
 }
